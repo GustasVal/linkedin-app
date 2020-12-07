cada18d0622bc8238e664be3dd84b715c95e296f
GustasValdavicius <gustas.valdavicius@hostinger.com>
2020-12-07 22:06:13 +0200

    Base Front-end setup

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,1 +1,16 @@
 require('./bootstrap');
+
+import App from './components/App';
+
+window.Vue = require('vue');
+window.Vuetify = require('vuetify');
+
+Vue.use(Vuetify);
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        App
+    },
+    vuetify: new Vuetify(),
+});
