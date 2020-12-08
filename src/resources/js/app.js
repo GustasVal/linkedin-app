3e2f7a7d715eb9adefb29f7bbc157e933c8d4b23
GustasValdavicius <gustas.valdavicius@hostinger.com>
2020-12-08 21:47:59 +0200

    Base Front-end application

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,16 +1,19 @@
 require('./bootstrap');
-
+import Vuetify from 'vuetify';
 import App from './components/App';
+import Drawer from "./components/Drawer";
+import Footer from './components/Footer';
 
 window.Vue = require('vue');
-window.Vuetify = require('vuetify');
 
 Vue.use(Vuetify);
 
 const app = new Vue({
     el: '#app',
     components: {
-        App
+        App,
+        Footer,
+        Drawer,
     },
     vuetify: new Vuetify(),
 });
--- src/resources/js/components/App.vue
+++ src/resources/js/components/App.vue
@@ -1,16 +1,35 @@
 <template>
-    <div>
-        {{ message }}
-    </div>
+    <v-app>
+        <v-navigation-drawer
+            v-model="drawer"
+            app
+        >
+            <Drawer />
+        </v-navigation-drawer>
+
+        <v-app-bar app>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+            <v-toolbar-title>Application</v-toolbar-title>
+        </v-app-bar>
+
+        <v-main>
+            <!--  -->
+        </v-main>
+
+        <Footer />
+    </v-app>
 </template>
 
 <script>
+import Footer from "./Footer";
+import Drawer from "./Drawer";
+
 export default {
-    computed: {},
-    data() {
-        return {
-            message:'Hello World'
-        }
-    }
-};
+    components: {
+        Footer,
+        Drawer
+    },
+    data: () => ({ drawer: false }),
+}
 </script>
